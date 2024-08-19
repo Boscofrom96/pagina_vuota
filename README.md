7be4bf8fa4e4cd2102812d2191b5eb23148715e0
Boscofrom96 <132386402+Boscofrom96@users.noreply.github.com>
2024-08-19 16:04:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pagina_vuota
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Boscofrom96/pagina_vuota)
---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOM completamente caricato e analizzato');
+
+  window.onload = function () {
+    console.log('Pagina completamente caricata');
+  };
+});
